3b16c4e08aba72818eb8d4f6926e8cdf8dcf0a8c
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-07-22 20:12:00 +0300

    stack

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,43 @@
+#Deliverables 
+#Modify your Stack __init__() method to take an optional limit value and set that as an attribute.
+#Update the Stack push() value to only push the passed-in value if there's still room in the Stack.
+#Stack size(): returns the number of elements contained in the Stack
+#Stack empty(): returns true if the Stack is empty; false otherwise
+#Stack full(): returns true if the Stack is full; false otherwise
+#Stack search(value): returns the distance between the top of the stack and the target element if it's present; -1 otherwise.
+
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+       self.items = items
+       self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
-    def push(self, item):
-        pass
+    def push(self, items):
+        if len(self.items)  < self.limit:
+            self.items.append(items)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        try:
+            index = self.items.index(target)
+            return len(self.items) - index - 1
+        except ValueError:
+            return -1
+
+        
